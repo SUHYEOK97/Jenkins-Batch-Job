458517b9dede36f6086bc389b8c80c0d6e246c82
SUHYEOK97 <rlatngur10@naver.com>
2023-02-05 20:01:23 +0900

    Project inti

---
+++ batch-job/src/main/java/com/data/batchjob/BatchJobApplication.java
@@ -0,0 +1,13 @@
+package com.data.batchjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchJobApplication.class, args);
+    }
+
+}
